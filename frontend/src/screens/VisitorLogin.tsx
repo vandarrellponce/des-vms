dad5eb51e9dbcbe4604893db7ad44e00ddbc9bc4
Van Darrell Ponce <riversideriver@yahoo.com>
2021-09-13 17:30:34 +0800

    redesigned visitorlogin screen

--- frontend/src/screens/VisitorLogin.tsx
+++ frontend/src/screens/VisitorLogin.tsx
@@ -5,91 +5,113 @@ import FormContainer from '../components/FormContainer/FormContainer'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Message from '../components/Message/Message'
+import './VisitorLogin.css'
 
 const VisitorLogin = () => {
-	const [result, setResult] = useState('')
-	const [meeting, setMeeting] = useState(null)
-	const [error, setError] = useState(null)
+  const [result, setResult] = useState('')
+  const [meeting, setMeeting] = useState(null)
+  const [error, setError] = useState(null)
 
-	useEffect(() => {
-		if (result) {
-			Axios.post('http://localhost:4000/api/visitor/login', {
-				token: result,
-			})
-				.then((res) => {
-					console.log(res.data)
-					setMeeting(res.data.meeting)
-				})
-				.catch((error) => {
-					const errorMessage = error.response?.data?.message
-						? error.response.data.message
-						: error.message
-					setError(errorMessage)
-				})
-		}
-	}, [result])
+  useEffect(() => {
+    if (result) {
+      Axios.post('http://localhost:4000/api/visitor/login', {
+        token: result,
+      })
+        .then((res) => {
+          console.log(res.data)
+          setMeeting(res.data.meeting)
+        })
+        .catch((error) => {
+          const errorMessage = error.response?.data?.message
+            ? error.response.data.message
+            : error.message
+          setError(errorMessage)
+        })
+    }
+  }, [result])
 
-	const handleScan = (data) => {
-		setResult(data)
-	}
+  const handleScan = (data) => {
+    setResult(data)
+  }
 
-	const handleError = (err) => {
-		console.error(err)
-	}
+  const handleError = (err) => {
+    console.error(err)
+  }
 
-	return (
-		<div>
-			<h2>Visitor Login</h2>
-			{!result ? (
-				<QrReader
-					delay={300}
-					onError={handleError}
-					onScan={handleScan}
-					style={{ width: '100%' }}
-				/>
-			) : (
-				<div>
-					{meeting ? (
-						<FormContainer>
-							<Form>
-								<Message
-									children={'STATUS OK'}
-									variant="success"
-								/>
-								<Form.Group>
-									<Form.Label>
-										Name of Person to Visit :{' '}
-										{meeting.appointment.personToVisit}
-									</Form.Label>
-								</Form.Group>
-								<Form.Group>
-									<Form.Label>
-										From : {meeting.appointment.start}{' '}
-									</Form.Label>
-									<br />
-								</Form.Group>
-								<Form.Group>
-									<Form.Label>
-										Until : {meeting.appointment.end}
-									</Form.Label>
-									<br />
-								</Form.Group>
-
-								<Button type="submit" variant="secondary">
-									Back
-								</Button>
-							</Form>
-						</FormContainer>
-					) : (
-						<Message
-							children={'Invalid / Expired passcode'}
-							variant="danger"
-						/>
-					)}
-				</div>
-			)}
-		</div>
-	)
+  return (
+    <div>
+      <h2>Visitor Login</h2>
+      {!result ? (
+        <QrReader
+          delay={300}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ width: '100%' }}
+        />
+      ) : (
+        <div>
+          {meeting ? (
+            <FormContainer>
+              <Form>
+                <Message children={'STATUS OK'} variant="success" />
+                Appointment Details:
+                <div className="visitor-login__meeting-details">
+                  <p>
+                    Name of Person to visit:{' '}
+                    <span>{meeting.appointment.personToVisit} </span>
+                  </p>
+                  <div className="visitor-login__time">
+                    <p>
+                      From{' '}
+                      <span>
+                        {' '}
+                        {new Date(
+                          meeting.appointment.start
+                        ).toLocaleDateString()}{' '}
+                        {new Date(
+                          meeting.appointment.start
+                        ).toLocaleTimeString()}{' '}
+                      </span>
+                    </p>
+                    <p>
+                      Until{' '}
+                      <span>
+                        {' '}
+                        {new Date(
+                          meeting.appointment.end
+                        ).toLocaleDateString()}{' '}
+                        {new Date(meeting.appointment.end).toLocaleTimeString()}
+                      </span>{' '}
+                    </p>
+                  </div>
+                </div>
+                Visitor Details:
+                <div className="visitor-login__visitor-details">
+                  <p>
+                    Name{' '}
+                    <span>
+                      {meeting.visitor.firstname} {meeting.visitor.lastname}{' '}
+                    </span>
+                  </p>
+                  <p>
+                    Address <span>{meeting.visitor.address}</span>
+                  </p>
+                  <p>
+                    Contact Number <span>{meeting.visitor.contact}</span>{' '}
+                  </p>
+                </div>
+                <Button type="submit" variant="secondary">
+                  Back
+                </Button>
+              </Form>
+            </FormContainer>
+          ) : (
+            <Message children={'Invalid / Expired passcode'} variant="danger" />
+          )}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default VisitorLogin
